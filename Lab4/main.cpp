d8b2f84304cb99fd9974dde1ad2c775b1c1ace4e
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-11-09 17:25:21 -0500

    Creando el menu.

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -6,9 +6,47 @@ using namespace std;
 
 int main()
 {
+  int opcion= -1;
+
+    while (opcion!=0) {
+        cout<<"*************************************"<<endl;
+        cout<<"*******Menu Red de enrutadores*******"<<endl;
+        cout<<"1. Crear Router"<<endl;
+        cout<<"2.Eliminar router"<<endl;
+        cout<<"3.Ruta mas corta, costo"<<endl;
+        cout<<"0. Salir"<<endl;
+        cin>>opcion;
+        switch(opcion){
+
+        case 1:
+        {
+            cout<<"Cree su router"<<endl;
+            break;
+        }
+        case 2:{
+
+            break;
+        }
+
+        case 3:{
+
+            break;
+
+        }
+
+
+        default:{
+
+
+                cout<<"opcion no valida"<<endl;
+            break;
+        }
+        }
+    }
+
     Enrutador A;
-    A.agregarEnrutador();
+   A.agregarEnrutador();
     A.mostrarEnrutador();
-    cout << "Hello World!" << endl;
+
     return 0;
 }
--- Lab4/red.h
+++ Lab4/red.h
@@ -3,6 +3,7 @@
 
 class Red
 {
+
 private:
 
 
